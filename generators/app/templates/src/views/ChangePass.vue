30cb4def4f1e063939dce83888eddc624e653e43
vagrant <vagrant@jhipster-devbox-5.1.0-Monbeck-RFQ>
2018-12-04 12:42:29 +0200

    potato

--- generators/app/templates/src/views/ChangePass.vue
+++ generators/app/templates/src/views/ChangePass.vue
@@ -18,7 +18,7 @@
                     <v-text-field ref='password' v-validate="{required: true, min: 4, max: 100}" prepend-icon="lock" name="password" :label="$t('app.cpassword.passwordLabel')" type="password" v-model="password"></v-text-field>
                     <v-alert :value="errors.has('password')" type="error">{{ errors.first('password') }}</v-alert>
 
-                    <v-text-field v-validate="{confirmed:'password'} "prepend-icon="lock" name="password2" :label="$t('app.cpassword.passwordConfirmLabel')" type="password"></v-text-field>
+                    <v-text-field v-validate="{confirmed:'password'} "prepend-icon="lock" name="password2" :label="$t('app.cpassword.passwordConfirmationLabel')" type="password"></v-text-field>
                     <v-alert :value="errors.has('password2')" type="error">{{ errors.first('password2') }}</v-alert>  
 
                     <v-alert type='success' :value="reset==true">{{$t('app.success')}}</v-alert>
