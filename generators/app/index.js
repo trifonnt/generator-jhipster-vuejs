b40efb123538e1f6256a0563b1d4480a35c195e7
vagrant <vagrant@jhipster-devbox-5.1.0-Monbeck-RFQ>
2018-12-17 17:32:48 +0200

    potato

--- generators/app/index.js
+++ generators/app/index.js
@@ -15,6 +15,7 @@ module.exports = class extends BaseGenerator {
             },
             readConfig() {
                 this.jhipsterAppConfig = this.getJhipsterAppConfig();
+                this.log(jhipsterAppConfig)
                 if (!this.jhipsterAppConfig) {
                     this.error('Can\'t read .yo-rc.json');
                 }
