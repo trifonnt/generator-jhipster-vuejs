730b6d9d64044ac05e14870722fb1dfb5d404c84
vagrant <vagrant@jhipster-devbox-5.1.0-Monbeck-RFQ>
2018-12-18 17:51:46 +0200

    potato

--- generators/app/templates/src/views/common/InputFile.vue
+++ generators/app/templates/src/views/common/InputFile.vue
@@ -2,24 +2,23 @@
 	<div>
     <input :name="name" :id='name' type="file" :multiple="multiple" @input="upload($event)" class='fileinput' :ref='name'/>
     <v-btn @click='chooseFile'>Upload</v-btn>
-    <span v-show='files.length'><v-chip v-for='file in files'>{{file.name}} <v-icon @click='remove(file)'>delete</v-icon></v-chip></span>
+    <span v-show='localFiles && localFiles.length'><v-chip v-for='file in localFiles'>{{file.name}} <v-icon @click='remove(file)'>delete</v-icon></v-chip></span>
 </div>
 </template>
 
 <script>
 	export default {
 		data: () => ({
-
+			localFiles: this.files
 		}),
 		methods: {
 			upload(e) {
-				this.files = [...e.target.files];
+				this.localFiles.push(...e.target.files);
 				this.$emit('upload', {files: e.target.files, name: e.target.name})
 			},
 			remove(file) {
-				console.log(this.files)
-				var index = this.files.indexOf(file);
-				this.files.splice(this.files, 1)
+				var index = this.localFiles.indexOf(file);
+				this.localFiles.splice(index, 1)
 			},
 			chooseFile() {
 				this.$refs[this.name].click();
