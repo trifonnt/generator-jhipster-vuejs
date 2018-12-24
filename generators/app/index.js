a73e0fd30763c89c1a76d5ea1c86cfe70ebe961d
vagrant <vagrant@jhipster-devbox-5.1.0-Monbeck-RFQ>
2018-12-24 16:25:58 +0200

    potato

--- generators/app/index.js
+++ generators/app/index.js
@@ -226,7 +226,8 @@ let destPath = this.options.env.cwd+'/'+'src/main/webapp/'+'/src';
             "luxon": "^1.8.2",
             "vue-datetime": "^1.0.0-beta.8",
             "weekstart": "^1.0.0",
-            "v-img": "^0.2.0"
+            "v-img": "^0.2.0",
+            "swagger-ui": "^3.20.4"
           },
           devDependencies: {
             "@babel/polyfill": "^7.0.0-beta.49",
