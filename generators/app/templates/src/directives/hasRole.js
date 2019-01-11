ad2b80bfb7ae6af067b9e6a2988561311dc0985d
vagrant <vagrant@jhipster-devbox-5.1.0-Monbeck-RFQ>
2019-01-11 16:46:44 +0200

    potato

--- generators/app/templates/src/directives/hasRole.js
+++ generators/app/templates/src/directives/hasRole.js
@@ -3,6 +3,15 @@ import Vue from 'vue'
 import {getProfile} from '../store/identity'
 
 Vue.directive('hasRole', {
+	inserted: function(el, b) {
+		if(b.value != '') {
+			if(!getProfile().jwt) el.style.display = 'none'
+			else if(b.value.includes(',') 
+				&& getProfile().jwt 
+				&& b.value.split(',').some(r=>!getProfile().roles.includes(r)).length==0) el.style.display = 'none'
+			else if(!b.value.includes(',') && getProfile().jwt && !getProfile().roles.includes(b.value)) el.style.display = 'none'			
+		}
+	},
 	update: function(el, b) {
 		if(b.value != '') {
 			console.log(getProfile(), "PROFILE")
