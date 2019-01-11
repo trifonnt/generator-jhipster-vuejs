f55e7387fe99a5deea90382beb186fc504b0fe0a
vagrant <vagrant@jhipster-devbox-5.1.0-Monbeck-RFQ>
2019-01-11 17:22:41 +0200

    potato

--- generators/app/templates/src/directives/hasRole.js
+++ generators/app/templates/src/directives/hasRole.js
@@ -12,6 +12,15 @@ Vue.directive('hasRole', {
 			else if(!b.value.includes(',') && getProfile().jwt && !getProfile().roles.includes(b.value)) el.style.display = 'none'			
 		}
 	},
+	componentUpdated: function(el, b) {
+		if(b.value != '') {
+			if(!getProfile().jwt) el.style.display = 'none'
+			else if(b.value.includes(',') 
+				&& getProfile().jwt 
+				&& b.value.split(',').some(r=>!getProfile().roles.includes(r)).length==0) el.style.display = 'none'
+			else if(!b.value.includes(',') && getProfile().jwt && !getProfile().roles.includes(b.value)) el.style.display = 'none'			
+		}
+	},
 	update: function(el, b) {
 		if(b.value != '') {
 			console.log(getProfile(), "PROFILE")
